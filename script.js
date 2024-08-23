e1cb0cf5f8f5aa27be9109a96a6bcfd85ffb00af
HectorMates <hector-cabello@hotmail.com>
2024-08-23 18:07:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ function calculateVAM() {
 
     const totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
 
+    let vam;
+
     if (distance > 0 && totalSeconds > 0) {
-        const vam = (distance / totalSeconds) * 3.6;
-        document.getElementById('vamValue').innerText = vam.toFixed(2);
+        const speed = (distance / totalSeconds) * 3.6;
+
+        if (distance > 42000) {
+            vam = -0.002617 * Math.pow(speed, 3) + 0.044267 * Math.pow(speed, 2) + 1.222151 * speed + 0.298783;
+        } else if (distance > 21000) {
+            vam = 0.026683 * Math.pow(speed, 3) - 1.012912 * Math.pow(speed, 2) + 13.805094 * speed - 49.963058;
+        } else if (distance > 14700) {
+            vam = -0.000352544 * Math.pow(speed, 3) - 0.00242428 * Math.pow(speed, 2) + 1.30944 * speed + 1.09206;
+        } else if (distance > 9800) {
+            vam = -0.001635 * Math.pow(speed, 3) + 0.034289 * Math.pow(speed, 2) + 1.009218 * speed + 0.96591;
+        } else if (distance > 4900) {
+            vam = -0.000527 * Math.pow(speed, 3) + 0.004781 * Math.pow(speed, 2) + 1.238002 * speed + 0.226654;
+        }
+
+	// Redondear hacia arriba y mostrar con un decimal
+        const roundedVAM = Math.ceil(vam * 10) / 10;
+        document.getElementById('vamValue').innerText = roundedVAM.toFixed(1);
     } else {
         alert("Por favor, introduce una distancia y tiempo válidos.");
     }
